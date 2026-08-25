49be9e70c47848899b0337bd1c16800c3872ec71
Hiroya Kubo <hiroya@cuc.ac.jp>
2026-08-26 00:22:03 +0900

    build: refresh committed extension artifact

--- dist/example-extension.js
+++ dist/example-extension.js
@@ -1,4 +1,4 @@
-// Name: Example Extension
+// Name: TurboWarp-Example-Extension
 // ID: kubohiroyaexampleextension
 // Description: A TypeScript-based TurboWarp extension.
 // By: Hiroya Kubo
@@ -8,9 +8,11 @@
   'use strict';
 
   const extensionConfig = {
-    id: "kubohiroyaexampleextension"
+    id: "kubohiroyaexampleextension",
+    docsURI: "https://kubohiroya.github.io/turbowarp-extension-template/",
+    blockIconURI: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0OCA0OCI+PHJlY3QgeD0iNCIgeT0iOCIgd2lkdGg9IjE4IiBoZWlnaHQ9IjE0IiByeD0iMyIgZmlsbD0iIzRDOTdGRiIvPjxyZWN0IHg9IjI2IiB5PSI4IiB3aWR0aD0iMTgiIGhlaWdodD0iMTQiIHJ4PSIzIiBmaWxsPSIjNTlDMDU5Ii8+PHJlY3QgeD0iMTUiIHk9IjI2IiB3aWR0aD0iMTgiIGhlaWdodD0iMTQiIHJ4PSIzIiBmaWxsPSIjRkZBQjE5Ii8+PC9zdmc+"
   };
-  const extensionName = "Example Extension";
+  const extensionName = "TurboWarp-Example-Extension";
   const blocks = [{ "opcode": "hello", "blockType": "REPORTER", "text": "hello [NAME]", "description": "Returns a localized greeting for the supplied name.", "arguments": { "NAME": { "type": "STRING", "defaultValue": "world" } } }];
   const definitions = {
     extensionName,
@@ -22,6 +24,8 @@
       return {
         id: extensionConfig.id,
         name: Scratch.translate(definitions.extensionName),
+        docsURI: extensionConfig.docsURI,
+        blockIconURI: extensionConfig.blockIconURI,
         blocks: blockDefinitions.map((block) => this.toScratchBlock(block))
       };
     }
